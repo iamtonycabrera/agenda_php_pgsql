f46f3b7a7823b2fe9f44bf0c512191b8b0f56556
Antonio Cabrera <acabrerag94@gmail.com>
2023-05-04 17:38:45 +0200

    Fixed user credential for access to database

--- conexion_postgress.php
+++ conexion_postgress.php
@@ -1,23 +1,23 @@
 <?php
-   
-    //Configurar datos de acceso a la Base de datos
-    $host = "localhost";
-    $dbname = "agenda1";
-    $dbuser = "postgres";
-    $userpass = "Admin123*";
-    
-    $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$dbuser;password=$userpass";
-    
-    try{
-     //Crear conexión a postgress
-     $conn = new PDO($dsn);
-    
-     //Mostgrar mensaje si la conexión es correcta
-     if($conn){
-     /* echo "Conectado a la base $dbname correctamente!"; */
-     echo "\n";
-     }
-    }catch (PDOException $e){
-     //Si hay error en la conexión mostrarlo
-     echo $e->getMessage();
+
+//Configurar datos de acceso a la Base de datos
+$host = "localhost";
+$dbname = "pgsql_agenda_db";
+$dbuser = "postgres";
+$userpass = "posgre";
+
+$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$dbuser;password=$userpass";
+
+try {
+    //Crear conexión a postgress
+    $conn = new PDO($dsn);
+
+    //Mostgrar mensaje si la conexión es correcta
+    if ($conn) {
+        /* echo "Conectado a la base $dbname correctamente!"; */
+        echo "\n";
     }
+} catch (PDOException $e) {
+    //Si hay error en la conexión mostrarlo
+    echo $e->getMessage();
+}
