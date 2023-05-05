5f7844517053edca90ea48bdb0593ae1fcac4e9b
Antonio Cabrera <acabrerag94@gmail.com>
2023-05-05 16:18:24 +0200

    Listed all categories from database

--- categorias.php
+++ categorias.php
@@ -1,107 +1,59 @@
-<?php include("includes/header.php") ?>
+<?php
+include("includes/header.php");
+
+// Configurar zona horaria
+date_default_timezone_set('Europe/Madrid');
+
+$query = "SELECT * FROM categorias";
+$stmt = $conn->query($query);
+
+$categorias = $stmt->fetchAll(PDO::FETCH_OBJ);
+// var_dump($categorias);
+
+
+?>
 
 <div class="row">
     <div class="col-sm-6">
         <h3>Lista de Categorías</h3>
-    </div> 
+    </div>
     <div class="col-sm-4 offset-2">
-        <a href="crear_categoria.php" class="btn btn-success w-100"><i class="bi bi-plus-circle-fill"></i> Nueva Categoría</a>
-    </div>    
+        <a href="crear_categoria.php" class="btn btn-success w-100"><i class="bi bi-plus-circle-fill"></i> Nueva
+            Categoría</a>
+    </div>
 </div>
 <div class="row mt-2 caja">
     <div class="col-sm-12">
-            <table id="tblCategorias" class="display" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Fecha de Creación</th>                       
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td>
-                            <a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a>                                                
+        <table id="tblCategorias" class="display" style="width:100%">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Fecha de Creación</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($categorias as $fila) : ?>
+                <tr>
+                    <td><?php echo $fila->id; ?></td>
+                    <td><?php echo $fila->nombre; ?></td>
+                    <td><?php echo $fila->fecha_creacion; ?></td>
+                    <td>
+                        <a href="editar_categoria.php?id=<?php echo $fila->id; ?>" class="btn btn-warning"><i
+                                class="bi bi-pencil-fill"></i>
+                            Editar</a>
                     </td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td><a href="editar_categoria.php" class="btn btn-warning"><i class="bi bi-pencil-fill"></i> Editar</a></td>
-                    </tr>                                          
-                </tbody>       
-            </table>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
     </div>
 </div>
 <?php include("includes/footer.php") ?>
 
 <script>
-    $(document).ready( function () {
-        $('#tblCategorias').DataTable();
-    });
+$(document).ready(function() {
+    $('#tblCategorias').DataTable();
+});
 </script>
